c045963322687dad1ca5b160887931fe2aca9fb1
alisson.marchiori1@gmail.com <alisson.marchiori1@gmail.com>
2022-06-09 14:35:47 +0000

    BLADO BLADO

--- models/staging/stg_categoris.sql
+++ models/staging/stg_categoris.sql
@@ -1,9 +1,9 @@
 with source_data as (
     select
-        category_id				
-        , category_name				
-        , 'description'	as category_description			
-        , picture				
+        category_id             
+        , category_name             
+        , 'description' as category_description         
+        , picture               
     from {{ source('northwind', 'categories') }}
 )
 
--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,18 +1,19 @@
 with source_data as (
     select
         product_id
-        , supplier_id				
-        , category_id				
-        , product_name								
-        , quantity_per_unit as qty_per_unit			
-        , unit_price			
-        , units_in_stock				
-        , units_on_order				
-        , cast(reorder_level as int) as reorder_level				
+        , supplier_id               
+        , category_id               
+        , product_name                              
+        , quantity_per_unit as qty_per_unit         
+        , unit_price            
+        , cast (units_in_stock as int) as   units_in_stock      
+        , units_on_order    
+        , cast(reorder_level as int) as reorder_level   /* convertendo a coluna em inteiro*/        
+
         , case
             when discontinued = 0 then 'No'  
             else 'Yes'
-        end as is_discontinued		
+        end as is_discontinued      
     from {{ source('northwind', 'products') }}
 )
 
